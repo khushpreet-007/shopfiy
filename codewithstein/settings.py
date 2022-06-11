8520dfe97e244de439be1f733cd92534c33b1904
Khushpreet Singh <75271300+khushpreet-007@users.noreply.github.com>
2022-06-11 11:51:26 +0530

    Update settings.py

--- codewithstein/settings.py
+++ codewithstein/settings.py
@@ -131,9 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_DIR, 'static'),
-)
+# STATICFILES_DIRS = (  os.path.join(PROJECT_DIR, 'static'), )
  
 django_heroku.settings(locals())
 
